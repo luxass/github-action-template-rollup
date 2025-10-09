017ab068b47cfb49a4ff78dd4e5dd84d7dc45100
Lucas <lucasnrgaard@gmail.com>
2025-10-09 03:53:53 +0200

    feat!: try migrating to esm

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -11,8 +11,8 @@ import typescript from "@rollup/plugin-typescript";
 export default {
   input: "src/index.ts",
   output: {
-    file: "dist/index.cjs",
-    format: "cjs",
+    file: "dist/index.js",
+    format: "esm",
     exports: "auto",
   },
   plugins: [
